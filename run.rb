46f6dd71547e5ba03fd2f269a5c3bffbc9b8bba9
Ashley <ashpow25@gmail.com>
2021-04-07 23:54:41 -0700

    added a clearing method

--- run.rb
+++ run.rb
@@ -3,10 +3,22 @@ require_relative 'config/environment.rb'
 CLI.new
 
 prompt = TTY::Prompt.new
+
+def clear
+    system('clear')
+end
+
 start = prompt.ask("Hey there!  What's your favorite coin?")
-puts "Really?  #{start} isn't a bad choice, but...do you want to hear some of the top to the MOON favorites?"
-coin = prompt.select
-puts "your favorite color is #{coin2}"
+
+clear
+
+puts "Really?  #{start} isn't a bad choice, but..."
+
+coin = prompt.yes?("do you want to hear some of the top to the MOON favorites?")
+
+
+clear
+#puts "your favorite color is #{coin2}"
 
 #binding.pry
 
